d56ea23ba4feaa5d9fac6aa704bb9f6e34beed99
HwangSukHyeon <payback9488@gmail.com>
2025-05-14 02:50:07 +0900

    [Proj]: Create Project

---
+++ SignIn&Up/SignIn&Up/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SignIn&Up
+//
+//  Created by 황석현 on 5/14/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ SignIn&Up/SignIn&UpTests/SignIn_UpTests.swift
@@ -0,0 +1,17 @@
+//
+//  SignIn_UpTests.swift
+//  SignIn&UpTests
+//
+//  Created by 황석현 on 5/14/25.
+//
+
+import Testing
+@testable import SignIn_Up
+
+struct SignIn_UpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
