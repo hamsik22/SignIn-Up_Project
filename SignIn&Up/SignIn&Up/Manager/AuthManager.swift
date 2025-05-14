4877ecdaf526df25c34c9e1c6759184fb2bc08af
HwangSukHyeon <payback9488@gmail.com>
2025-05-14 14:36:49 +0900

    [Code]: Setup AuthManager DataLogics
    - 추후 구현할 함수들을 나열

--- SignIn&Up/SignIn&Up/Manager/AuthManager.swift
+++ SignIn&Up/SignIn&Up/Manager/AuthManager.swift
@@ -5,10 +5,36 @@
 //  Created by 황석현 on 5/14/25.
 //
 
-import Foundation
+import CoreData
+import UIKit
 
 class AuthManager {
     
     weak var delegate: AuthManagable?
     
+    private var context: NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    // MARK: 회원가입
+    func signUp() { }
+    
+    // MARK: 이메일 중복 확인
+    func isEmailDuplicated() -> Bool { return false }
+    
+    // MARK: 로그인
+    func login() { }
+    
+    // MARK: 로그아웃
+    func logOut() { }
+    
+    // MARK: 계정 삭제
+    func deleteAccount() { }
+    
+    // MARK: 로그인 상태 확인
+    func isLoggedIn() { }
+    
+    // MARK: 현재 로그인된 사용자 정보 가져오기
+    func getCurrentUser() { }
 }
