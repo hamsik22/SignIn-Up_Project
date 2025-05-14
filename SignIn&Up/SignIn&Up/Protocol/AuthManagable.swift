c377329c627c1bf33f4d702e20bd68a7d5146944
HwangSukHyeon <payback9488@gmail.com>
2025-05-14 14:44:45 +0900

    [Code]: Add Method in AuthManagable

--- SignIn&Up/SignIn&Up/Protocol/AuthManagable.swift
+++ SignIn&Up/SignIn&Up/Protocol/AuthManagable.swift
@@ -9,5 +9,18 @@ import Foundation
 
 protocol AuthManagable: AnyObject {
     
+    // MARK: 회원가입 관련
+    func didSignUpSuccess()
+    func didSignUpFailure(_ error: String)
     
+    // MARK: 로그인 관련
+    func didLoginSuccess()
+    func didLoginFailure(_ error: String)
+    
+    // MARK: 로그아웃 관련
+    func didLogout()
+    
+    // MARK: 계정 삭제
+    func didDeleteAccountSuccess()
+    func didDeleteAccountFailure(_ error: String)
 }
